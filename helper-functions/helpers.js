65910330ae69d84151ccc7e70dd5de691516c52c
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-17 17:19:29 +0000

    Initial helper functions folder

---
+++ helper-functions/helpers.js
@@ -0,0 +1,13 @@
+// Random short url generator to be used by long url
+const generateRandomString = () => {
+  let rand = "";
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 7; i++) {
+    rand += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return rand;
+};
+
+
+module.exports = {generateRandomString}
