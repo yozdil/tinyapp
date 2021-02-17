eaa068be706cb475b810069d89b479428b40bc06
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-17 20:02:08 +0000

    validate function

--- helper-functions/helpers.js
+++ helper-functions/helpers.js
@@ -9,5 +9,21 @@ const generateRandomString = () => {
   return rand;
 };
 
+const validate = (email, password, data) => {
+  let userObj = Object.values(data);
+  const currentUser = userObj.find((user) => user.email === email);
+  if (currentUser) {
+    if (currentUser.password === password) {
+      // successful login
+      return { user: currentUser, error: null };
+    } else {
+      // failed at password
+      return { user: null, error: "password" };
+    }
+  } else {
+    // failed at email
+    return { user: null, error: "email" };
+  }
+};
 
-module.exports = {generateRandomString}
+module.exports = { generateRandomString, validate };
