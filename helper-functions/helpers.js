f9cad1005a03cb6725e2c8221aef7ad26cd137f6
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-18 18:28:05 +0000

    Modified the generated user ID length

--- helper-functions/helpers.js
+++ helper-functions/helpers.js
@@ -5,7 +5,7 @@ const generateRandomString = () => {
   let rand = "";
   const chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 6; i++) {
     rand += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return rand;
